e216d2cff5e1882c89e7a6c4e0d69fcf654f103c
AAZemour <aa.zemour@gmail.com>
2023-11-16 11:35:28 +0100

    Modifications du header

--- header.php
+++ header.php
@@ -9,21 +9,29 @@
 </head>
 
 <body>
-    <header class="header">
-        <div>
-            <img class="logo" src="wp-content/themes/MotaPhoto/assets/images/Logo.png" alt="Logo Nathalie Mota"></img>
-        </div>
-        <div class="menu">
-            <?php
-            if (has_nav_menu('header-menu')): ?>
+    <header>
+        <div class="logo-nav">
+            <div class="logo">
+                <img src="wp-content/themes/MotaPhoto/assets/images/Logo.png" alt="Logo Nathalie Mota"></img>
+            </div>
+            <div class="menu">
                 <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'header-menu',
-                        'menu_class' => 'my-header-menu',
-                    )
-                ); ?>
-            <?php endif;
-            ?>
+                if (has_nav_menu('header-menu')): ?>
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'header-menu',
+                            'menu_class' => 'my-header-menu',
+                        )
+                    ); ?>
+                <?php endif;
+                ?>
+            </div>
+        </div>
+        <div>
+            <img class="image-titre" src="wp-content/themes/MotaPhoto/assets/images/nathalie-11.jpeg"
+                alt="Photo d'un événement"></img>
+            <img class="titre-site" src="wp-content/themes/MotaPhoto/assets/images/PHOTOGRAPHE EVENT.png"
+                alt="Photographe event"></img>
         </div>
     </header>
